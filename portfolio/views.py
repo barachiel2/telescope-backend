e900c76c1858ea6e9ca759bc9978a9cae1cb7231
Luka Barisic <lbarishich@gmail.com>
2024-10-23 11:07:04 +0200

    initial model view serializer setup.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Portfolio
+from .serializers import PortfolioSerializer
 
-# Create your views here.
+class PortfolioViewSet(viewsets.ModelViewSet):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+    permission_classes = [IsAuthenticated]
+
+    # Custom action for additional functionality, if needed
+    # @action(detail=True, methods=['POST'])
+    # def custom_action(self, request, pk=None):
+    #     # custom logic here
+    #     return Response({'status': 'custom action'})
