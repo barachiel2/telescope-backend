0d72e095ee1f13cd1b8e586334a9aec6c091be98
Luka Barisic <lbarishich@gmail.com>
2024-10-24 12:28:33 +0200

    property viewset.

--- portfolio/views/property.py
+++ portfolio/views/property.py
@@ -1,9 +1,44 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from ..models import Property
 from ..serializers import PropertySerializer
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
-    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['POST'])
+    def create_property(self, request):
+        serializer = PropertySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['GET'])
+    def retrieve_property(self, request, pk=None):
+        property = self.get_object()
+        serializer = PropertySerializer(property)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PUT', 'PATCH'])
+    def update_property(self, request, pk=None):
+        property = self.get_object()
+        serializer = PropertySerializer(property, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['DELETE'])
+    def delete_property(self, request, pk=None):
+        property = self.get_object()
+        property.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=['GET'])
+    def list_properties(self, request):
+        properties = Property.objects.all()
+        serializer = PropertySerializer(properties, many=True)
+        return Response(serializer.data)
