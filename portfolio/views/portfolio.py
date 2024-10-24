e7c63f733e3fda3305e1e1b158e21e29439df2be
Luka Barisic <lbarishich@gmail.com>
2024-10-24 10:43:49 +0200

    cors + portfolio view functions.

--- portfolio/views/portfolio.py
+++ portfolio/views/portfolio.py
@@ -1,9 +1,46 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from ..models import Portfolio
 from ..serializers import PortfolioSerializer
 
 class PortfolioViewSet(viewsets.ModelViewSet):
     queryset = Portfolio.objects.all()
     serializer_class = PortfolioSerializer
-    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['POST'])
+    def create_portfolio(self, request):
+        serializer = PortfolioSerializer(data=request.data)
+        if serializer.is_valid():
+            # You can handle owner in other ways if needed
+            # e.g., manually passing an owner or ignoring it
+            serializer.save()  # We skip setting an owner since no user is authenticated
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['GET'])
+    def retrieve_portfolio(self, request, pk=None):
+        portfolio = self.get_object()
+        serializer = PortfolioSerializer(portfolio)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PUT', 'PATCH'])
+    def update_portfolio(self, request, pk=None):
+        portfolio = self.get_object()
+        serializer = PortfolioSerializer(portfolio, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['DELETE'])
+    def delete_portfolio(self, request, pk=None):
+        portfolio = self.get_object()
+        portfolio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=['GET'])
+    def list_portfolios(self, request):
+        portfolios = Portfolio.objects.all()  # No filtering by user since there's no authentication
+        serializer = PortfolioSerializer(portfolios, many=True)
+        return Response(serializer.data)
--- telescope_backend/settings.py
+++ telescope_backend/settings.py
@@ -30,11 +30,13 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'portfolio',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -117,3 +119,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
